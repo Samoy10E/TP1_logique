cbebbd94cee1c91118b4b86cd6a959a5bbd1f4f9
Samoy10E <90957900+Samoy10E@users.noreply.github.com>
2021-10-22 16:27:26 +0200

    Generators (#2)
    * TP 15/10
    
    * tp 22/10

--- View.py
+++ View.py
@@ -1,35 +1,58 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import List, Callable, Dict, Tuple, Union
 
+"""
+Abstract class, manage views and controls
+"""
 class View(ABC):
 
-    nomProblemeCourant: string
-    problemeCourant: clauseToLit
+    nomProblemeCourant: str
+    problemeCourant: List[List[int]]
 
-    nomHeuristiqueCourante: string
-    heuristiqueCourante: fonction
+    nomHeuristiqueCourante: str
+    heuristiqueCourante: Callable
 
-    problemeAnalyse: dictionnary #{nomProbleme_nomHeuristiqe_taille : (temps_exec, nombre_noeud,model)}
+    problemeAnalyse: Dict[str, Tuple[int, int, Union[bool, List[List[int]]]]] #{nomProbleme_nomHeuristiqe_taille : (temps_exec, nombre_noeud,model)}
     #Le modèle est un bool si on choisi l'option estSatifiable, non?
 
     def __init__(self):
-        return
+        self.ProblemeCrourant = []
+        self.nomProblemeCourant = ""
 
-    def select_fichier(self,nomFichier: string) -> clauseToInt:
-        #TODO méthode commune
-        return
+    def select_fichier(self,nomFichier: str) -> List[List[int]]:
+        fileName = r"formula/" + nomFichier + ".txt"
+        fileObj = Path(fileName)
+        if not fileObj.is_file():
+            raise NameError(fileName)
+        file = open(fileName)
+        texte = file.readlines()
+        clauses = []
+        for ligne in texte:
+            if ligne[0] != "c" and ligne[0] != "p":
+                prop = ligne.split(" ")
+                clause = []
+                for p in prop:
+                    p = int(p)*2
+                    if p<0:
+                        p = -p+1
+                    clause.append(p)
+                clauses.append(clause)
+        self.ProblemeCrourant = clauses
+        self.nomProblemeCourant = nomFichier
 
-    def select_generateur(self, nomGenerateur: fonction, *args) -> clauseToInt:
-        #TODO méthode commune
-        return
+    def select_generateur(self, nomFonctionGenerateur: Callable, nomGenerateur: str, n: int) -> List[List[int]]:
+        self.ProblemeCrourant = nomFonctionGenerateur(n)
+        self.nomProblemeCourant = nomGenerateur
 
-    def select_heuristique(self,heuristique : fonction):
+    def select_heuristique(self,heuristique : Callable):
         self.heuristiqueCourante = heuristique
 
-    def lanceSolveur(self, satisfiableOuSolution : bool) -> bool_ou_model:
+    def lance_solveur(self, satisfiableOuSolution : bool) -> Union[bool, List[List[int]]]:
         #TODO méthode commune
         return
 
-    def analyse(self, taille):
+    def analyse(self, taille: int):
         temps_exec = 0
         nombre_noeud = 0
         model = []
@@ -46,7 +69,7 @@ class View(ABC):
         return
 
     @abstractmethod
-    def affiche(self,nomProbleme_nomHeuristiqe_taille: string):
+    def affiche(self,nomProbleme_nomHeuristiqe_taille: str):
         #TODO en fonction de model (bool ou non) affiche:
         #TODO affiche temps d'execution
         #TODO affiche nombre de noeud
@@ -59,7 +82,17 @@ class View(ABC):
 
 class ViewConsole(View):
 
-    def affiche(self,nomProbleme_nomHeuristiqe_taille: string):
+    def action(self):
+        print(">")
+        entre = input()
+        print(entre)
+        if entre in ["sortie","-s","exit"]:
+            return False
+        elif entre in ["aide","help"]:
+            print("Pour sortir taper 'sortie'")
+        return True
+
+    def affiche(self,nomProbleme_nomHeuristiqe_taille: str):
         # TODO en fonction de model (bool ou non) affiche:
         # TODO affiche temps d'execution
         # TODO affiche nombre de noeud
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from generators import pigeonProblem
+from View import ViewConsole
 
 def main():
-    #print(pigeonProblem.generate_clauses(3,4))
 
-    pigeonProblem.generate_file(10)
+    pigeonProblem.generate_file(3)
+    VC = ViewConsole()
+    test = True
+    while test:
+        test = VC.action()
 
 if __name__ == '__main__':
     main()
