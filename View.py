88c3e57384994fb0aefd016c9582775988c12f44
Samoy10E <90957900+Samoy10E@users.noreply.github.com>
2021-10-22 16:19:32 +0200

    tp 22/10

--- View.py
+++ View.py
@@ -17,23 +17,38 @@ class View(ABC):
     #Le modèle est un bool si on choisi l'option estSatifiable, non?
 
     def __init__(self):
-        return
+        self.ProblemeCrourant = []
+        self.nomProblemeCourant = ""
 
     def select_fichier(self,nomFichier: str) -> List[List[int]]:
         fileName = r"formula/" + nomFichier + ".txt"
         fileObj = Path(fileName)
         if not fileObj.is_file():
             raise NameError(fileName)
-        return
+        file = open(fileName)
+        texte = file.readlines()
+        clauses = []
+        for ligne in texte:
+            if ligne[0] != "c" and ligne[0] != "p":
+                prop = ligne.split(" ")
+                clause = []
+                for p in prop:
+                    p = int(p)*2
+                    if p<0:
+                        p = -p+1
+                    clause.append(p)
+                clauses.append(clause)
+        self.ProblemeCrourant = clauses
+        self.nomProblemeCourant = nomFichier
 
-    def select_generateur(self, nomGenerateur: Callable, *args) -> List[List[int]]:
-        #TODO méthode commune
-        return
+    def select_generateur(self, nomFonctionGenerateur: Callable, nomGenerateur: str, n: int) -> List[List[int]]:
+        self.ProblemeCrourant = nomFonctionGenerateur(n)
+        self.nomProblemeCourant = nomGenerateur
 
     def select_heuristique(self,heuristique : Callable):
         self.heuristiqueCourante = heuristique
 
-    def lanceSolveur(self, satisfiableOuSolution : bool) -> Union[bool, List[List[int]]]:
+    def lance_solveur(self, satisfiableOuSolution : bool) -> Union[bool, List[List[int]]]:
         #TODO méthode commune
         return
 
@@ -66,6 +81,16 @@ class View(ABC):
         pass
 
 class ViewConsole(View):
+
+    def action(self):
+        print(">")
+        entre = input()
+        print(entre)
+        if entre in ["sortie","-s","exit"]:
+            return False
+        elif entre in ["aide","help"]:
+            print("Pour sortir taper 'sortie'")
+        return True
 
     def affiche(self,nomProbleme_nomHeuristiqe_taille: str):
         # TODO en fonction de model (bool ou non) affiche:
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from View import ViewConsole
 def main():
 
     pigeonProblem.generate_file(3)
+    VC = ViewConsole()
+    test = True
+    while test:
+        test = VC.action()
 
 if __name__ == '__main__':
     main()
